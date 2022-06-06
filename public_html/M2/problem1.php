4d14602494a653f48ebd9ef502083af29b1c8182
Saumya Dwivedi <saumyad03@gmail.com>
2022-06-05 21:18:40 -0400

    completed hw

--- public_html/M2/problem1.php
+++ public_html/M2/problem1.php
@@ -8,7 +8,11 @@ function processArray($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Odds output:<br>";
     //TODO add logic here to echo out only odd values
-
+    foreach($arr as $val) {
+        if ($val % 2 == 1) {
+            echo $val . "<br>";
+        }
+    }
 }
 echo "Problem 1: Odd Output<br>";
 ?>
--- public_html/M2/problem3.php
+++ public_html/M2/problem3.php
@@ -8,7 +8,9 @@ function bePositive($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Positive output:<br>";
     //TODO use echo to output all of the values as positive (even if they were originally positive)
-
+    foreach($arr as $val) {
+        echo ($val >= 0 ? $val : -$val) . "<br>";
+    }
 }
 echo "Problem 3: Be Positive<br>";
 ?>
