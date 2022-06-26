b1e2fc117c11795e8e3fa87d1ab47cbcce0e7dca
Saumya Dwivedi <saumyad03@gmail.com>
2022-06-26 14:39:21 -0400

    helper functions

--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -37,9 +37,14 @@ require_once(__DIR__ . "/../../partials/nav.php");
             $hasError = true;
         }
         //sanitize
-        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        $email = sanitize_email($email);
         //validate
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        /*if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "Please enter a valid email <br>";
+            $hasError = true;
+        }*/
+        if (!is_valid_email($email)) {
             echo "Please enter a valid email <br>";
             $hasError = true;
         }
