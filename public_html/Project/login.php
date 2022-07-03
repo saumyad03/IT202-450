9148d428eaa50fd645a2c1904d47238d7ecb25f3
Saumya Dwivedi <saumyad03@gmail.com>
2022-07-03 15:25:00 -0400

    user profile stuff

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -54,7 +54,7 @@ require_once(__DIR__ . "/../../partials/nav.php");
         if (!$hasError) {
             //TODO 4
             $db = getDB();
-            $stmt = $db->prepare("SELECT email, password from Users where email = :email");
+            $stmt = $db->prepare("SELECT id, email, username, password from Users where email = :email");
             try {
                 $r = $stmt->execute([":email" => $email]);
                 if ($r) {
