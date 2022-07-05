a1564a9fcd5b7b8a394ed80c47cc9b2335e80b26
Saumya Dwivedi <saumyad03@gmail.com>
2022-07-05 17:26:48 -0400

    add username and email login instead of just email

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -3,8 +3,8 @@ require_once(__DIR__ . "/../../partials/nav.php");
 ?>
 <form onsubmit="return validate(this)" method="POST">
     <div>
-        <label for="email">Email</label>
-        <input type="email" name="email" required />
+        <label for="email">Email/Username</label>
+        <input type="text" name="email" required />
     </div>
     <div>
         <label for="pw">Password</label>
@@ -16,7 +16,7 @@ require_once(__DIR__ . "/../../partials/nav.php");
     function validate(form) {
         //TODO 1: implement JavaScript validation
         //ensure it returns false for an error and true for success
-        
+        //let email = form.email.value
         return true;
     }
 </script>
@@ -31,17 +31,19 @@ require_once(__DIR__ . "/../../partials/nav.php");
             flash("Email must not be empty <br>");
             $hasError = true;
         }
-        //sanitize
-        //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
-        $email = sanitize_email($email);
-        //validate
-        /*if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            flash("Please enter a valid email <br>");
-            $hasError = true;
-        }*/
-        if (!is_valid_email($email)) {
-            flash("Please enter a valid email <br>");
-            $hasError = true;
+        if (str_contains($email, "@")) {
+            //sanitize
+            $email = sanitize_email($email);
+            //validate
+            if (!is_valid_email($email)) {
+                flash("Invalid email address");
+                $hasError = true;
+            }
+        } else {
+            if (!is_valid_username($email)) {
+                flash("Invalid username");
+                $hasError = true;
+            }
         }
         if (empty($password)) {
             flash("Password must not be empty <br>");
@@ -54,7 +56,7 @@ require_once(__DIR__ . "/../../partials/nav.php");
         if (!$hasError) {
             //TODO 4
             $db = getDB();
-            $stmt = $db->prepare("SELECT id, email, username, password from Users where email = :email");
+            $stmt = $db->prepare("SELECT id, email, username, password from Users where email = :email or username=:email");
             try {
                 $r = $stmt->execute([":email" => $email]);
                 if ($r) {
