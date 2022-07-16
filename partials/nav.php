50d0e6d489ab14214d8cf6c58bc0329a3163e7b6
Saumya Dwivedi <saumyad03@gmail.com>
2022-07-16 02:33:54 -0400

    adding products

--- partials/nav.php
+++ partials/nav.php
@@ -57,6 +57,9 @@ session_start();
                         </ul>
                     </li>
                 <?php endif; ?>
+                <?php if (has_role("Admin") || has_role("Shop Owner")) : ?>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo get_url('add_product.php'); ?>">Add Product</a><li>
+                <?php endif; ?>
                 <?php if (is_logged_in()) : ?>
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
                 <?php endif; ?>
